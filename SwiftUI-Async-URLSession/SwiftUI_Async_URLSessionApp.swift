dd729ed3510f385682a767f0b74c5bd5af1f29bc
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-14 13:20:03 -0600

    Initial Commit

---
+++ SwiftUI-Async-URLSession/SwiftUI_Async_URLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Async_URLSessionApp.swift
+//  SwiftUI-Async-URLSession
+//
+//  Created by R. Mark Volkmann on 11/14/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Async_URLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
